1b7a86c815a0e91df88e804ebf28e788976bd5d2
CCP Aporia <28982391+ccp-aporia@users.noreply.github.com>
2025-08-26 13:47:34 +0000

    Add missing nullptr checks
    Currently, on TQ there is a corp description that contains "invalid" Unicode characters. These characters cause the underlying sys call to fail. There was no guard for this on macOS, so logic would proceed with a nullptr that then resulted in a crash.
    
    Fixes https://ccpgames.atlassian.net/browse/PLAT-9947

--- WrapPointList_Blue.cpp
+++ WrapPointList_Blue.cpp
@@ -157,8 +157,16 @@ PyObject* Py__init__(PyObject *self, PyObject *args)
 	}
 #elif defined( __APPLE__ )
     pThis->m_coreString = ToStringRef( reinterpret_cast<const wchar_t*>( textStr ), size_t( textLength ) );
+    if ( ! pThis->m_coreString ) {
+        PyErr_SetString( PyExc_SystemError, "Text analysis failed, cannot convert to string." );
+        return NULL;
+    }
     AutoReleaseCF<CFStringRef> name = ToStringRef( LanguageIDToCode( CodeToLanguageID( langStr ) ) );
     pThis->m_coreLocale = CFLocaleCreate( nullptr, name );
+    if ( ! pThis->m_coreLocale ) {
+        PyErr_SetString( PyExc_SystemError, "Text analysis failed, cannot create locale." );
+        return NULL;
+    }
 #endif
 	Py_RETURN_NONE;
 };
