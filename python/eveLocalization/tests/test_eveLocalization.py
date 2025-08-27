ff908e79ab9813b290e3ae5c79c35c113b90c2e2
CCP Aporia <28982391+ccp-aporia@users.noreply.github.com>
2025-08-27 20:34:13 +0000

    Add relevant test case
    This resembles what caused the crash in https://ccpgames.atlassian.net/browse/PLAT-9947

--- python/eveLocalization/tests/test_eveLocalization.py
+++ python/eveLocalization/tests/test_eveLocalization.py
@@ -523,6 +523,16 @@ class LocalizationUnittests(unittest.TestCase):
         d = el.WrapPointList(s, "en-us")
         self.assertFalse(d.GetLinebreakPoints())
 
+    def testWrapPointsForTextWithInvalidUnicodeCharacters(self):
+        """ See https://ccpgames.atlassian.net/browse/PLAT-9947
+        """
+        # the sample text was extracted from the input that caused the crash
+        text = u' \ud83d Spieler, die Lust auf Mining, Produktion oder PVE haben'
+        with self.assertRaises(SystemError):
+            # construction now raises a SystemError instead of silently creating a nullptr
+            d = el.WrapPointList(text, "en-us")
+            # this line should no longer get executed, but it is what caused the crash initially
+            d.GetLinebreakPoints()
 
 if __name__ == '__main__':
     unittest.main()
